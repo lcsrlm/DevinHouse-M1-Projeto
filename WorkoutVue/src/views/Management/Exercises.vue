795376a42c825e96b8fe5e0db5766a68e251aaf3
lcsrlm <lucas_rosa-lima@estudante.sesisenai.org.br>
2023-09-02 14:23:56 -0300

    feat: adiciona design da página de exercicios

--- WorkoutVue/src/views/Management/Exercises.vue
+++ WorkoutVue/src/views/Management/Exercises.vue
@@ -1,3 +1,16 @@
 <template>
-    exercicios
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="12" md="7">
+                <v-card class="pa-4" elevation="3">
+                    <h2>Gerenciamento de Exercícios</h2>
+                    <v-form>
+                        <v-text-field label="Nome do Exercício">
+                        </v-text-field>
+                        <v-btn color="primary">Cadastrar</v-btn>
+                    </v-form>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
