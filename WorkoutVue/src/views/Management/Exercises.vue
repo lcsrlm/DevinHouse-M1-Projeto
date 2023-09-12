f0f39b0106e7d22379d06d66c2eb199d90e69c88
lcsrlm <lucas_rosa-lima@estudante.sesisenai.org.br>
2023-09-12 18:30:40 -0300

    fix : correção do arquivo de exercicios

--- WorkoutVue/src/views/Management/Exercises.vue
+++ WorkoutVue/src/views/Management/Exercises.vue
@@ -1,3 +1,77 @@
 <template>
-    exercicios
+    <v-layout>
+    <v-main>
+    <v-container fluid>
+        <v-row align="center" justify="center" style="height: 85vh">
+            <v-col cols="15" sm="6" md="7">
+                <v-card elevation="3">
+                    <v-card-title class="text-h5">Gerenciamento de Exercícios</v-card-title>
+                    <v-form @submit.prevent="cadastrarExercicio" ref="exercise-form">
+                        <v-text-field v-model="novoExercicio.description" label="Nome do Exercício" outlined
+                            :rules="[v => !!v || '']"></v-text-field>
+                        <div class="text-center mt-2">
+                            <v-btn type="submit" color="primary">Cadastrar</v-btn>
+                        </div>
+                    </v-form>
+                    <v-list>
+                        <v-list-item v-for="(exercicio, id) in exercicios" :key="id">
+                            <v-list-item-content>
+                                <v-list-item-title>{{ exercicio.description }}</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</v-main>
+</v-layout>
 </template>
+  
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            novoExercicio: {
+                description: '',
+            },
+            exercicios: [],
+        }
+    },
+    mounted() {
+        this.carregarExercicios()
+    },
+    methods: {
+        carregarExercicios() {
+            axios.get('http://localhost:3000/exercises')
+                .then((response) => {
+                    this.exercicios = response.data
+                })
+                .catch((error) => {
+                    console.error('Erro ao carregar exercícios:', error)
+                });
+        },
+        async cadastrarExercicio() {
+            const { valid } = await this.$refs['exercise-form'].validate()
+
+            if (!valid) {
+                alert("Preencha o nome do exercício!")
+                return
+            }
+
+            axios.post('http://localhost:3000/exercises', this.novoExercicio)
+                .then(() => {
+                    alert('Exercício cadastrado com sucesso!')
+                    this.$refs['exercise-form'].reset()
+                    this.carregarExercicios()
+                })
+                .catch((error) => {
+                    console.error('Erro ao cadastrar exercício:', error)
+                })
+        }
+    }
+}
+</script>
+  
