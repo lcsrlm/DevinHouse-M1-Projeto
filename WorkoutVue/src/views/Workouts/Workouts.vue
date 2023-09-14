726775d69c2dff6a4acf227e57723f3724f94501
lcsrlm <lucas_rosa-lima@estudante.sesisenai.org.br>
2023-09-14 19:53:07 -0300

    feat: salva o nome do aluno utilizando o map

--- WorkoutVue/src/views/Workouts/Workouts.vue
+++ WorkoutVue/src/views/Workouts/Workouts.vue
@@ -4,9 +4,10 @@
             <v-row justify="center" align="center" style="height: 10vh;">
                 <v-col cols="12" md="8">
                     <v-card elevation="3">
-                        <v-card-title class="pa-5" align="center">Treinos - {{ alunoId }} </v-card-title>
+                        <v-card-title class="pa-5" align="center">Treinos - {{ alunoNome }}</v-card-title>
                         <v-card-text>
                             {{ workoutsData }}
+                           
                         </v-card-text>
                     </v-card>
                 </v-col>
@@ -22,17 +23,29 @@ export default {
     data() {
         return {
             workoutsData: [],
+            alunoNome: ''
         }
     },
 
     props: ['alunoId'],
    
-    async mounted() {
+   mounted() {
+   this.buscarDados()
+},
+methods: {
+  async buscarDados () {
     try {
         const response = await axios.get(`http://localhost:3000/workouts?student_id=${this.alunoId}`)
-        this.workoutsData = response.data
+        this.workoutsData = response.data.workouts
+        this.alunoNome = this.pegarAlunoNome(response.data)
+        
     } catch (error) {
         console.error('Erro ao buscar treinos:', error)
+    }
+  },
+  pegarAlunoNome(data) {
+      const alunoNome = data.workouts.map(workout => workout.student_name)[0] 
+      return alunoNome || ''
     }
 }
 
